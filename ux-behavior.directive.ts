97f4fa9e137cf9f940e6a3289ae472b5cd2a0b14
Arthur Khachatryan <arthurk55@gmail.com>
2018-06-02 08:20:29 -0700

    Added ux category specification, cleanup and documentation

--- ux-behavior.directive.ts
+++ ux-behavior.directive.ts
@@ -7,23 +7,24 @@ export class UxBehaviorDirective implements OnInit {
 
 	readonly directiveName = 'UX Behavior Directive';
 	readonly supportedEvents = [
-		{ type: 'click', kind: 'native', source: 'mouse' },
-		{ type: 'mouseover', kind: 'native', source: 'mouse' },
-		{ type: 'mouseout', kind: 'native', source: 'mouse' },
-		{ type: 'keyup', kind: 'native', source: 'keyboard' },
-		{ type: 'kedown', kind: 'native', source: 'keyboard' }
+		{ action: 'click', kind: 'native', source: 'mouse' },
+		{ action: 'mouseover', kind: 'native', source: 'mouse' },
+		{ action: 'mouseout', kind: 'native', source: 'mouse' },
+		{ action: 'keyup', kind: 'native', source: 'keyboard' },
+		{ action: 'kedown', kind: 'native', source: 'keyboard' }
 	];
 
-	// add interfaces for each vendor
-	private supportedVendors = [
+	readonly supportedVendors = [
 		{
 			name: 'Google Analytics',
 			method: this.sendGoogleAnalyticsEvent
 		}
 	]
 
-	@Input() uxType: any;
-	@Input() uxName: any;
+	@Input() uxAction: string;
+	@Input() uxLabel: string;
+	@Input() uxCategory: string;
+
 	constructor(
 		public el: ElementRef
 	) { }
@@ -32,48 +33,62 @@ export class UxBehaviorDirective implements OnInit {
 		this.setBehavior();
 	}
 
+	/**
+	 * Checks if the behavior is supported, and if so, directs the 
+	 * setting of the behavior to the specific method to handle that
+	 * type of behavior.
+	 */
 	private setBehavior() {
-		let uxEvent = this.supportedEvents.find(obj => obj.type === this.uxType);
+		let supportedEvent = this.supportedEvents.find(obj => obj.action === this.uxAction);
 
-		if (!uxEvent) {
-			return console.error(this.directiveName + ' does not support event type of ' + this.uxType);
+		if (!supportedEvent) {
+			return console.error(this.directiveName + ' does not support event type of ' + this.uxAction);
 		}
 
-		// @todo remove once the first proprietary event is added 
-		if (uxEvent.kind !== 'native') {
+		// fail fast, currently supporting native behaviors only
+		if (supportedEvent.kind !== 'native') {
 			return console.error(this.directiveName + ' does not currently support proprietary events.');
 		}
 
-		switch (uxEvent.source) {
+		switch (supportedEvent.source) {
 			case 'mouse':
 			case 'keyboard':
-				return this.setMouseKeyboardBehavior(uxEvent.source, uxEvent.type, this.uxName);
+				return this.setMouseKeyboardBehavior(supportedEvent.action, this.uxLabel, this.uxCategory);
 		}
 
 		// if ux event is not caught by now, it is not supported
-		return console.error(this.directiveName + ' does not currently support the kind of event you are trying to track.');		
+		return console.error(this.directiveName + ' does not currently support the kind of event you are trying to track.');
 	}
 
-	private setMouseKeyboardBehavior(source: string, type: string, name: string) {
+	/**
+	 * Sets the mouse and keyboard listeners for the element of interest
+	 * @param supportedAction the supported action we want to record
+	 * @param label the UX label passed into the directive
+	 * @param category the UX category passed into the directive
+	 */
+	private setMouseKeyboardBehavior(supportedAction: string, label: string, category: string) {
 		let instance = this;
-		this.el.nativeElement.addEventListener(type, function() {
+		this.el.nativeElement.addEventListener(supportedAction, function() {
 			// loop through supported vendors and call the event 'send' method
 			instance.supportedVendors.forEach((vendor) => {
 				// we're using separate methods for firing event methods for each vendor
 				// since each one may have specific implementation
-				vendor.method(source, type, name);
+				vendor.method(supportedAction, label, category);
 			});
 		});
 	}
 
-	// vendor-specific event integrations
-	// @todo these should be moved out into their own files at some point
-
-	private sendGoogleAnalyticsEvent(source: string, type: string, name: string) {
+	/**
+	 * Google Analytics vendor-specific event integration
+	 * @param supportedAction the supported action we want to record
+	 * @param label the UX label passed into the directive
+	 * @param category the UX category passed into the directive
+	 */
+	private sendGoogleAnalyticsEvent(supportedAction: string, label: string, category: string) {
 		console.log('Google Analytics event firing...');
-		(<any>window).gtag('event', type, {
-			'event_label': source,
-			'event_category': name
+		(<any>window).gtag('event', supportedAction, {
+			'event_label': label,
+			'event_category': category
 		});
 	}
 
